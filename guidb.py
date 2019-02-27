2ea9959f95eb0c50880246e67791996b51d31c41
devyogilogic <yogeshyadavcsk@gmail.com>
2019-02-27 05:25:06 -0800

    Add files via upload

---
+++ guidb.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+import  tkinter.messagebox
+import time
+
+import mysql
+import mysql.connector as myc
+myobj=myc.connect(user="root",password="",database="guidb")
+cusrorobj=myobj.cursor()
+window=tk.Tk()
+loginid=tk.StringVar()
+start_time=time.time()
+loginpass=tk.StringVar()
+def logintime():
+
+    sec = '%.f' % (time.time() - start_time)
+
+    sec = int(sec)
+    a = time.strftime('%H:%M:%S', time.gmtime(sec))
+
+    tkinter.messagebox.showinfo("Time", a)
+    arg = (str(a),loginid.get(), loginpass.get())
+    cusrorobj.execute("update users set time=%s where  userid=%s and password= %s", arg)
+    cusrorobj.execute("commit")
+    print(a)
+
+def newwindow():
+    window2 = tk.Tk()
+    tk.Button(window2, text="log out", command=logintime).pack()
+def inserttoddb():
+    try:
+        arg=(loginid.get(),loginpass.get())
+        print(arg)
+        cusrorobj.execute("insert into  users(userid,password)values (%s,%s)",arg)
+        cusrorobj.execute("commit")
+        tkinter.messagebox.showinfo("Info", "User added successfully")
+
+    except myc.errors.IntegrityError:
+        tkinter.messagebox.showinfo("Info", "User already exsits")
+
+
+def logincheck():
+    try:
+
+        arg = (loginid.get(), loginpass.get())
+        print(arg)
+        cusrorobj.execute("select * from users where userid=%s and password=%s", arg)
+        for i in cusrorobj:
+            li=[]
+            li.append(i)
+            print(len(tuple(li[0])))
+
+        a=len(tuple(li[0]))
+        if(a==4):
+            start_time = time.time()
+            newwindow()
+    except UnboundLocalError :
+        tkinter.messagebox.showinfo("Info", "Wrong Crendials")
+
+
+
+
+tk.Label(window,text="Enter your id").grid(row=0,column=3)
+tk.Entry(window,textvariable=loginid,bd=5).grid(row=0,column=5)
+tk.Label(window,text="Enter your password").grid(row=1,column=3)
+tk.Entry(window,textvariable=loginpass,bd=5).grid(row=1,column=5)
+tk.Button(window,text="Signup",command=lambda:inserttoddb()).grid(row=3,column=4)
+tk.Button(window,text="Login" ,command=lambda :logincheck()).grid(row=5,column=4)
+
+window.mainloop()
+
+
+
+
+
+
+
---
+++ guidb.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 27, 2019 at 02:22 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `guidb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `userid` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `time` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `userid`, `password`, `time`) VALUES
+(13, 'yogesh', '123', '00:00:17');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `userid` (`userid`,`password`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
